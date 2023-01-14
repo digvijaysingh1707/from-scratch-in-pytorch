f7b1e3a8ace27e1ac747cf104e08a409780cf11f
Harsh Agrawal <harshagrawal.1312@gmail.com>
2023-01-14 17:31:27 +0000

    feat: add word2vec

--- rnn/char_lstm_torch.py
+++ rnn/char_lstm_torch.py
@@ -18,6 +18,8 @@ from tqdm import tqdm
 
 if torch.cuda.is_available():
     device = "cuda"
+elif torch.backends.mps.is_available():
+    device = "mps"
 else:
     device = "cpu"
 print(f"Using Device: {device}")
